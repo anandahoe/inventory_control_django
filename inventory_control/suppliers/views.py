9cf509120888597d51f66ead0aaeeb938e3e4e54
anandahoe <145071722+anandahoe@users.noreply.github.com>
2024-01-27 16:14:59 -0300

    Aula 2

--- inventory_control/suppliers/views.py
+++ inventory_control/suppliers/views.py
@@ -1,14 +1,66 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Supplier
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from .forms import SupplierForm
 
 # Create your views here.
 def index(request):
     suppliers = Supplier.objects.order_by("-id")
     
-    context = {"suppliers": suppliers}
+    #Aplicando a paginação
+    paginator = Paginator(suppliers, 100)
+    # Obtendo a página da URL
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
+    context = {"suppliers": page_obj}
     
     return render(request, "index.html", context)
+
+def search(request):
+    #Obtendo o valor da requisição (Formulário)
+    search_value = request.GET.get("q").strip()
+    
+    #Verificando se algo foi digitado
+    if not search_value:
+        return redirect("suppliers:index")
+    
+    #Filtando os fornecedores
+    # O Q é usando para combinar filtros (& ou |) - (and ou or)
+    suppliers = Supplier.objects \
+        .filter(Q(fantasy_name__icontains=search_value) |
+                Q(company_name__icontains=search_value)) \
+        .order_by("-id")
+    
+    paginator = Paginator(suppliers, 100)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
+    context = { "suppliers": page_obj }
+    
+    return render(request, "index.html", context)
+    
+def create(request):
+    #POST
+    if request.method == 'POST':
+        form = SupplierForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect("suppliers:index")
+        
+        context = { "form": form }
+        
+        return render(request, "create.html", context)
+    
+    #GET
+    form = SupplierForm()
+    
+    context = { "form": form }
+    return render(request, "create.html", context)
 
 @require_POST
 def delete(request, id):
@@ -16,4 +68,12 @@ def delete(request, id):
     supplier.delete()
     
     return redirect("suppliers:index")
+
+@require_POST
+def toggle_enabled(request, id):
+    supplier = get_object_or_404(Supplier, pk=id)
     
+    supplier.enabled = not supplier.enabled
+    supplier.save()
+    
+    return JsonResponse({ "message": "success" })
